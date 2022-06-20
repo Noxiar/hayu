4e30eb344ffa831ef7ddbd53b1c534debb04b621
Noxiar <107659769+Noxiar@users.noreply.github.com>
2022-06-20 08:15:53 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -47,10 +47,10 @@ export default function App() {
             <main className="lex flex-col items-center justify-left  sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="flex flex-col items-center justify-center lg:text-center">
                 <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">Data to enrich your</span> <span className="block text-indigo-600 xl:inline">online business</span>
+                  <span className="block xl:inline">Nonton FIlm</span> <span className="block text-indigo-600 xl:inline">online business</span>
                 </h1>
                 <p className="mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                 Gratis Disini
                 </p>
               </div>
 
